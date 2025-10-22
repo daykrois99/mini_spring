34e235213788ee8c029a26cf42f3de98037f754a
daykrois <daykrois99@outlook.com>
2025-10-22 22:21:28 +0800

    增加bean定义类、Component注解、ApplicationContext的各种方法(初始化上下文、扫描包、初始化bean、创建bean、是否包含Component注解) 测试扫描包功能是否正常

--- src/main/java/com/example/ApplicationContext.java
+++ src/main/java/com/example/ApplicationContext.java
@@ -1,19 +1,52 @@
 package com.example;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 
 public class ApplicationContext {
 
-    public ApplicationContext(String packageName) {
+    public ApplicationContext(String packageName) throws IOException {
         initContext(packageName);
     }
 
 
-    public void initContext(String packageName) {
-        List<Class<?>> componentClassList = scanPackage(packageName).stream().filter(c -> c.isAnnotationPresent(Component.class)).toList();
+    public void initContext(String packageName) throws IOException {
+        scanPackage(packageName).stream().filter(this::scanCreate).map(this::wrapper).forEach(this::createBean);
     }
 
-    public List<Class<?>> scanPackage(String packageName) {
+    protected boolean scanCreate(Class<?> type) {
+        return type.isAnnotationPresent(Component.class);
+    }
+
+    protected void createBean(BeanDefinition beanDefinition) {
+
+    }
+
+    protected BeanDefinition wrapper(Class<?> type) {
+        return new BeanDefinition(type);
+    }
+
+
+    public List<Class<?>> scanPackage(String packageName) throws IOException {
+        URL resource = this.getClass().getClassLoader().getResource(packageName.replace(".", File.separator));
+        Path path = Path.of(resource.getFile());
+        Files.walkFileTree(path, new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Path absolutePath = file.toAbsolutePath();
+                if (absolutePath.toString().endsWith(".class")) {
+                    System.out.println(absolutePath);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
         return null;
     }
 
---
+++ src/main/java/com/example/BeanDefinition.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import java.lang.reflect.Constructor;
+
+public class BeanDefinition {
+    public BeanDefinition(Class<?> type) {
+
+    }
+
+    public String getName() {
+        return "";
+    }
+
+    public Constructor<?> getConstructor() {
+        return null;
+    }
+}
